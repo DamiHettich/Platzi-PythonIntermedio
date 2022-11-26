e6d0ee9658f87e80eaa25ff8bfac9003a5b5b14c
Damian Hettich <dhettich@brainfood.cl>
2022-11-26 02:52:07 -0300

    basics of lists and dictionaries

---
+++ 01_basics.py
@@ -0,0 +1,17 @@
+
+###### VIRTUAL ENVIRONMENTS #######
+# python3 -m venv nombre_venv
+
+# .\venv\Scripts\activate           <- Windows
+# source venv/bin/activate          <- Unix
+# deactivate
+
+# alias nombre-alias="comando"      
+#   alias avenv=“source venv/bin/activate”
+
+####### PIP (PACKAGE INSTALLER FOR PYTHON) ######
+
+# pip freeze >> requirements.txt
+# pip install package-name
+# pip install -r requirements.txt
+
